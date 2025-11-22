32c3b0102990666c92d47620b010c48197e2fcb8
aleksei <aleksei@example.com>
2025-11-22 15:22:23 +0300

    update22112u4

--- lib/main.dart
+++ lib/main.dart
@@ -69,10 +69,8 @@ class _StockPriceScreenState extends State<StockPriceScreen> {
   final List<Map<String, String>> _stocksInfo = [
     {'ticker': 'X5', 'name': 'X5', 'lotSize': '1'},
     {'ticker': 'NVTK', 'name': 'НОВАТЭК', 'lotSize': '1'},
-    {'ticker': 'GMKN', 'name': 'Норникель', 'lotSize': '10'},
     {'ticker': 'OZON', 'name': 'OZON', 'lotSize': '1'},
     {'ticker': 'MDMG', 'name': 'Мать и Дитя', 'lotSize': '1'},
-    {'ticker': 'OZPH', 'name': 'ОзонФарм', 'lotSize': '10'},
     {'ticker': 'PLZL', 'name': 'Полюс', 'lotSize': '1'},
     {'ticker': 'SBERP', 'name': 'Сбербанк', 'lotSize': '1'},
     {'ticker': 'CHMF', 'name': 'Северсталь', 'lotSize': '1'},

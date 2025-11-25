8b64ad746609a2cbb08f6b34e32fc609a3b284a1
aleksei <aleksei@example.com>
2025-11-25 10:45:13 +0300

    upf25

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ class _StockPriceScreenState extends State<StockPriceScreen> {
     {'ticker': 'TATNP', 'name': 'Татнефть', 'lotSize': '1'},
     {'ticker': 'PHOR', 'name': 'ФосАгро', 'lotSize': '1'},
     {'ticker': 'YDEX', 'name': 'Yandex', 'lotSize': '1'},
-    {'ticker': 'T', 'name': 'Т-Технологии', 'lotSize': '1'},
+    {'ticker': 'MOEX', 'name': 'МосБиржа', 'lotSize': '10'},
   ];
 
   List<Stock> _stocks = [];

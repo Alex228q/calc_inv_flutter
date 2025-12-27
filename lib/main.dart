b7c0ea873b59993c1bb950c56b5490ac864722cf
aleksei <aleksei@example.com>
2025-12-27 19:37:03 +0300

    update2712

--- lib/main.dart
+++ lib/main.dart
@@ -73,11 +73,13 @@ class _StockPriceScreenState extends State<StockPriceScreen> {
     {'ticker': 'X5', 'name': 'X5', 'lotSize': '1'},
     {'ticker': 'LKOH', 'name': 'Лукойл', 'lotSize': '1'},
     {'ticker': 'MDMG', 'name': 'Мать и дитя', 'lotSize': '1'},
+    {'ticker': 'MOEX', 'name': 'Московская Биржа', 'lotSize': '10'},
     {'ticker': 'NVTK', 'name': 'Новатэк', 'lotSize': '1'},
     {'ticker': 'GMKN', 'name': 'Норникель', 'lotSize': '10'},
     {'ticker': 'OZON', 'name': 'OZON', 'lotSize': '1'},
     {'ticker': 'PLZL', 'name': 'Полюс', 'lotSize': '1'},
     {'ticker': 'SBERP', 'name': 'Сбербанк', 'lotSize': '1'},
+    {'ticker': 'CHMF', 'name': 'Северсталь', 'lotSize': '1'},
     {'ticker': 'PHOR', 'name': 'Фосагро', 'lotSize': '1'},
     {'ticker': 'YDEX', 'name': 'Yandex', 'lotSize': '1'},
   ];

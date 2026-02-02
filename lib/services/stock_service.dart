ffbdbb89b0eebc7af2283855e499233ffe6a4238
aleksei <aleksei@example.com>
2026-02-02 17:08:28 +0300

    up0202

--- lib/services/stock_service.dart
+++ lib/services/stock_service.dart
@@ -4,12 +4,17 @@ import '../models/stock.dart';
 
 class StockService {
   static const List<Map<String, String>> _stocksInfo = [
+    {'ticker': 'X5', 'name': 'X5', 'lotSize': '1'},
     {'ticker': 'MDMG', 'name': 'Мать и дитя', 'lotSize': '1'},
     {'ticker': 'NVTK', 'name': 'Новатэк', 'lotSize': '1'},
+    {'ticker': 'GMKN', 'name': 'Норникель', 'lotSize': '10'},
     {'ticker': 'PLZL', 'name': 'Полюс', 'lotSize': '1'},
     {'ticker': 'SBERP', 'name': 'Сбербанк', 'lotSize': '1'},
+    {'ticker': 'CHMF', 'name': 'Северсталь', 'lotSize': '1'},
+    {'ticker': 'TATNP', 'name': 'Татнефть', 'lotSize': '1'},
     {'ticker': 'PHOR', 'name': 'Фосагро', 'lotSize': '1'},
     {'ticker': 'YDEX', 'name': 'Yandex', 'lotSize': '1'},
+
   ];
 
   Future<List<Stock>> fetchStockPrices() async {

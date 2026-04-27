de98851b3ff76206140ef5897ff34a838b007cd5
aleksei <aleksei@example.com>
2026-04-27 13:08:27 +0300

    finish270426_2

--- lib/services/stock_service.dart
+++ lib/services/stock_service.dart
@@ -8,7 +8,9 @@ class StockService {
   static const List<Map<String, String>> _stocksInfo = [
     {'ticker': 'X5', 'name': 'X5', 'lotSize': '1'},
     {'ticker': 'MDMG', 'name': 'Мать и дитя', 'lotSize': '1'},
+    {'ticker': 'MOEX', 'name': 'Мосбиржа', 'lotSize': '10'},
     {'ticker': 'NVTK', 'name': 'Новатэк', 'lotSize': '1'},
+    {'ticker': 'GMKN', 'name': 'Норникель', 'lotSize': '10'},
     {'ticker': 'PLZL', 'name': 'Полюс', 'lotSize': '1'},
     {'ticker': 'SBERP', 'name': 'Сбербанк', 'lotSize': '1'},
     {'ticker': 'TATNP', 'name': 'Татнефть', 'lotSize': '1'},

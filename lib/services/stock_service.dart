4f65a9f1e683c477cb5ae38eecfd1822ac7a9a41
aleksei <aleksei@example.com>
2026-04-28 12:57:54 +0300

    finish280426

--- lib/services/stock_service.dart
+++ lib/services/stock_service.dart
@@ -11,9 +11,11 @@ class StockService {
     {'ticker': 'MOEX', 'name': 'Мосбиржа', 'lotSize': '10'},
     {'ticker': 'NVTK', 'name': 'Новатэк', 'lotSize': '1'},
     {'ticker': 'GMKN', 'name': 'Норникель', 'lotSize': '10'},
+    {'ticker': 'OZON', 'name': 'OZON', 'lotSize': '1'},
     {'ticker': 'PLZL', 'name': 'Полюс', 'lotSize': '1'},
     {'ticker': 'SBERP', 'name': 'Сбербанк', 'lotSize': '1'},
     {'ticker': 'TATNP', 'name': 'Татнефть', 'lotSize': '1'},
+    {'ticker': 'TRNFP', 'name': 'Транснефть', 'lotSize': '1'},
     {'ticker': 'PHOR', 'name': 'Фосагро', 'lotSize': '1'},
     {'ticker': 'YDEX', 'name': 'Yandex', 'lotSize': '1'},
   ];

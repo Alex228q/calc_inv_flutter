487d6f4e709c5eb215e23fa26eb052ad1dccb3a7
Alex228q <113701334+Alex228q@users.noreply.github.com>
2026-07-21 20:20:25 +0300

    210726f

--- lib/services/stock_service.dart
+++ lib/services/stock_service.dart
@@ -6,38 +6,52 @@ import '../models/stock.dart';
 
 class StockService {
   static const List<Map<String, dynamic>> _stocksInfo = [
-    {"ticker": "X5", "name": "Икс 5", "lotSize": "1", "targetPercentage": 15.0},
+    {"ticker": "X5", "name": "Икс 5", "lotSize": "1", "targetPercentage": 10.0},
 
+    {
+      "ticker": "MOEX",
+      "name": "Мосбиржа",
+      "lotSize": "10",
+      "targetPercentage": 12.0,
+    },
     {
       "ticker": "NVTK",
       "name": "Новатэк",
       "lotSize": "1",
-      "targetPercentage": 15.0,
+      "targetPercentage": 10.0,
     },
+
     {
       "ticker": "OZON",
       "name": "OZON",
       "lotSize": "1",
-      "targetPercentage": 17.0,
+      "targetPercentage": 10.0,
     },
     {
       "ticker": "PLZL",
       "name": "Полюс",
       "lotSize": "1",
-      "targetPercentage": 15.0,
+      "targetPercentage": 10.0,
     },
     {
       "ticker": "SBERP",
       "name": "Сбербанк",
       "lotSize": "1",
-      "targetPercentage": 20.0,
+      "targetPercentage": 18.0,
+    },
+
+    {
+      "ticker": "TATNP",
+      "name": "Татнефть",
+      "lotSize": "1",
+      "targetPercentage": 13.0,
     },
 
     {
       "ticker": "YDEX",
       "name": "Yandex",
       "lotSize": "1",
-      "targetPercentage": 18.0,
+      "targetPercentage": 17.0,
     },
   ];
 

bd386407803bca44eca9c66b8edaea0d20fb0288
Alex228q <113701334+Alex228q@users.noreply.github.com>
2026-08-11 16:49:44 +0300

    110826

--- lib/services/stock_service.dart
+++ lib/services/stock_service.dart
@@ -6,60 +6,47 @@ import '../models/stock.dart';
 
 class StockService {
   static const List<Map<String, dynamic>> _stocksInfo = [
-    {"ticker": "X5", "name": "Икс 5", "lotSize": "1", "targetPercentage": 7},
-    {"ticker": "LKOH", "name": "Лукойл", "lotSize": "1", "targetPercentage": 8},
+    {"ticker": "X5", "name": "Икс 5", "lotSize": "1", "targetPercentage": 10},
     {
-      "ticker": "MDMG",
-      "name": "Мать и дитя",
+      "ticker": "LKOH",
+      "name": "Лукойл",
       "lotSize": "1",
-      "targetPercentage": 7,
+      "targetPercentage": 14,
     },
-    {
-      "ticker": "MOEX",
-      "name": "Мосбиржа",
-      "lotSize": "10",
-      "targetPercentage": 8,
-    },
+
     {
       "ticker": "NVTK",
       "name": "Новатэк",
       "lotSize": "1",
-      "targetPercentage": 8,
+      "targetPercentage": 14,
     },
     {
       "ticker": "GMKN",
       "name": "Норникель",
       "lotSize": "10",
-      "targetPercentage": 7,
+      "targetPercentage": 8,
     },
-    {"ticker": "OZON", "name": "OZON", "lotSize": "1", "targetPercentage": 7},
-    {"ticker": "PLZL", "name": "Полюс", "lotSize": "1", "targetPercentage": 8},
+    {"ticker": "OZON", "name": "OZON", "lotSize": "1", "targetPercentage": 10},
 
     {
       "ticker": "SBERP",
       "name": "Сбербанк",
       "lotSize": "1",
-      "targetPercentage": 9,
+      "targetPercentage": 18,
     },
-    {
-      "ticker": "CHMF",
-      "name": "Северсталь",
-      "lotSize": "1",
-      "targetPercentage": 7,
-    },
-    {
-      "ticker": "TATNP",
-      "name": "Татнефть",
-      "lotSize": "1",
-      "targetPercentage": 8,
-    },
+
     {
       "ticker": "PHOR",
       "name": "Фосагро",
       "lotSize": "1",
-      "targetPercentage": 7,
+      "targetPercentage": 8,
     },
-    {"ticker": "YDEX", "name": "Yandex", "lotSize": "1", "targetPercentage": 9},
+    {
+      "ticker": "YDEX",
+      "name": "Yandex",
+      "lotSize": "1",
+      "targetPercentage": 18,
+    },
   ];
 
   // Метод для получения целевых процентов

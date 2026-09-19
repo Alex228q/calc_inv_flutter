80574734cb226413eb62b91b5595be5035408c23
loki <lena45130@gmail.com>
2026-09-19 16:08:31 +0300

    190926

--- lib/services/stock_service.dart
+++ lib/services/stock_service.dart
@@ -8,25 +8,31 @@ class StockService {
   static const List<Map<String, dynamic>> _stocksInfo = [
    
     
-    {"ticker": "X5", "name": "Икс 5", "lotSize": "1", "targetPercentage": 8},
+    {"ticker": "X5", "name": "Икс 5", "lotSize": "1", "targetPercentage": 7},
     {"ticker": "MDMG", "name": "Мать и дитя", "lotSize": "1", "targetPercentage": 7},
    
     {
       "ticker": "MOEX",
       "name": "Мосбиржа",
       "lotSize": "10",
-      "targetPercentage": 8,
+      "targetPercentage": 7,
     },
 
     {
       "ticker": "NVTK",
       "name": "Новатэк",
       "lotSize": "1",
-      "targetPercentage": 8,
+      "targetPercentage": 7,
     },
   
+    {
+      "ticker": "GMKN",
+      "name": "Норникель",
+      "lotSize": "10",
+      "targetPercentage": 7,
+    },
   
-    {"ticker": "OZON", "name": "OZON", "lotSize": "1", "targetPercentage": 8},
+    {"ticker": "OZON", "name": "OZON", "lotSize": "1", "targetPercentage": 7},
     {"ticker": "PLZL", "name": "Полюс", "lotSize": "1", "targetPercentage": 7},
 
     
@@ -35,14 +41,19 @@ class StockService {
       "ticker": "SBERP",
       "name": "Сбербанк",
       "lotSize": "1",
-      "targetPercentage": 12,
+      "targetPercentage": 8,
     },
- 
-    {"ticker": "TATNP", "name": "Татнефть", "lotSize": "1", "targetPercentage": 8},
-    {"ticker": "T", "name": "Т-Технологии", "lotSize": "1", "targetPercentage": 8},
+  {
+      "ticker": "CHMF",
+      "name": "Северсталь",
+      "lotSize": "1",
+      "targetPercentage": 7,
+    },
+    {"ticker": "T", "name": "Т-Технологии", "lotSize": "1", "targetPercentage": 7},
+    {"ticker": "TATNP", "name": "Татнефть", "lotSize": "1", "targetPercentage": 7},
     
     {"ticker": "PHOR", "name": "Фосагро", "lotSize": "1", "targetPercentage": 7},
-    {"ticker": "YDEX", "name": "Yandex", "lotSize": "1", "targetPercentage": 12},
+    {"ticker": "YDEX", "name": "Yandex", "lotSize": "1", "targetPercentage": 8},
 
     
   ];
